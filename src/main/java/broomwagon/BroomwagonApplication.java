8f912ac28789a6b0fef43cd49c2877574b6891ff
Vladmir Parshikov <v-vparshikov@expedia.com>
2015-04-23 17:53:04 +0100

    Initial commit

---
+++ src/main/java/broomwagon/BroomwagonApplication.java
@@ -0,0 +1,12 @@
+package broomwagon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BroomwagonApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BroomwagonApplication.class, args);
+    }
+}
