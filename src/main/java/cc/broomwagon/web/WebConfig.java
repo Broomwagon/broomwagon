b279328ae77808ca002cb40e56048e817953ae15
Vladmir Parshikov <v-vparshikov@expedia.com>
2015-04-24 14:13:55 +0100

    added thymeleaf

---
+++ src/main/java/cc/broomwagon/web/WebConfig.java
@@ -0,0 +1,32 @@
+package cc.broomwagon.web;
+
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class WebConfig {
+//    @Bean
+//    public ThymeleafViewResolver viewResolver(){
+//        ThymeleafViewResolver viewResolver = new ThymeleafViewResolver();
+//        viewResolver.setTemplateEngine(thymeleafEngine());
+//        viewResolver.setCharacterEncoding("UTF-8");
+//        viewResolver.setOrder(0);
+//        return viewResolver;
+//    }
+//
+//    @Bean
+//    public SpringTemplateEngine thymeleafEngine() {
+//        SpringTemplateEngine thymeleafEngine = new SpringTemplateEngine();
+//        thymeleafEngine.setTemplateResolver(templateResolver());
+//        return thymeleafEngine;
+//    }
+//
+//    @Bean
+//    public ServletContextTemplateResolver templateResolver() {
+//        ServletContextTemplateResolver templateResolver = new ServletContextTemplateResolver();
+//        templateResolver.setPrefix("/resources/templates");
+//        templateResolver.setSuffix(".html");
+//        templateResolver.setTemplateMode("HTML5");
+//        return templateResolver;
+//    }
+}
---
+++ src/main/java/cc/broomwagon/web/controller/HomeController.java
@@ -0,0 +1,19 @@
+package cc.broomwagon.web.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Map;
+
+/**
+ * Controller to display homepage.
+ */
+@Controller
+public class HomeController {
+
+    @RequestMapping("/")
+    public String home(Map<String, Object> model) {
+        model.put("testOne", "yo yo 2");
+        return "home";
+    }
+}
