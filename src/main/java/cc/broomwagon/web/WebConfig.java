e26b78cfd610f7232455473af2987b4a2397bea5
Vladmir Parshikov <v-vparshikov@expedia.com>
2015-04-24 18:04:16 +0100

    experimenting with themes

--- src/main/java/cc/broomwagon/web/WebConfig.java
+++ src/main/java/cc/broomwagon/web/WebConfig.java
@@ -1,32 +1,29 @@
 package cc.broomwagon.web;
 
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.ui.context.ThemeSource;
+import org.springframework.ui.context.support.ResourceBundleThemeSource;
+import org.springframework.web.servlet.ThemeResolver;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.theme.FixedThemeResolver;
 
 @Configuration
-public class WebConfig {
-//    @Bean
-//    public ThymeleafViewResolver viewResolver(){
-//        ThymeleafViewResolver viewResolver = new ThymeleafViewResolver();
-//        viewResolver.setTemplateEngine(thymeleafEngine());
-//        viewResolver.setCharacterEncoding("UTF-8");
-//        viewResolver.setOrder(0);
-//        return viewResolver;
-//    }
-//
-//    @Bean
-//    public SpringTemplateEngine thymeleafEngine() {
-//        SpringTemplateEngine thymeleafEngine = new SpringTemplateEngine();
-//        thymeleafEngine.setTemplateResolver(templateResolver());
-//        return thymeleafEngine;
-//    }
-//
-//    @Bean
-//    public ServletContextTemplateResolver templateResolver() {
-//        ServletContextTemplateResolver templateResolver = new ServletContextTemplateResolver();
-//        templateResolver.setPrefix("/resources/templates");
-//        templateResolver.setSuffix(".html");
-//        templateResolver.setTemplateMode("HTML5");
-//        return templateResolver;
-//    }
+public class WebConfig extends WebMvcConfigurerAdapter {
+
+    @Bean
+    public ThemeSource themeSource() {
+        ResourceBundleThemeSource themeSource = new ResourceBundleThemeSource();
+        themeSource.setBasenamePrefix("themes/");
+        return themeSource;
+    }
+
+    @Bean
+    public ThemeResolver themeResolver () {
+        FixedThemeResolver fixedThemeResolver = new FixedThemeResolver();
+        fixedThemeResolver.setDefaultThemeName("default");
+        return fixedThemeResolver;
+    }
+
 }
