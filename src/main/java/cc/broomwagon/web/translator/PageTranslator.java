e4b46a0d27e063f26170205beb8400e03809a797
Vladmir Parshikov <vladimirus@gmail.com>
2015-06-10 16:46:36 +0100

    added page translator

---
+++ src/main/java/cc/broomwagon/web/translator/PageTranslator.java
@@ -0,0 +1,38 @@
+package cc.broomwagon.web.translator;
+
+import static java.util.stream.Collectors.toList;
+import static java.util.stream.StreamSupport.stream;
+
+import cc.broomwagon.model.page.Page;
+import cc.broomwagon.model.page.Row;
+import cc.broomwagon.model.page.Segment;
+import cc.broomwagon.web.ui.PageForm;
+import org.springframework.stereotype.Component;
+
+/**
+ * Class to translate between page and page form.
+ */
+@Component
+public class PageTranslator {
+
+    public Page translate(PageForm form) {
+        return Page.builder()
+                .id(form.getId())
+                .title(form.getTitle())
+                .url(form.getUrl())
+                .rows(stream(form.getRows().spliterator(), false)
+                                .map(row -> Row.builder()
+                                        .segments(stream(row.getSegments().spliterator(), false)
+                                                .map(segment -> Segment.builder()
+                                                        .id(segment.getId())
+                                                        .template(segment.getTemplate())
+                                                        .fragment(segment.getFragment())
+                                                        .cssClass(segment.getCssClass())
+                                                        .parameters(segment.getParameters())
+                                                        .build())
+                                                .collect(toList()))
+                                        .build())
+                                .collect(toList())
+                ).build();
+    }
+}
---
+++ src/test/java/cc/broomwagon/web/translator/PageTranslatorTest.java
@@ -0,0 +1,59 @@
+package cc.broomwagon.web.translator;
+
+import static com.google.common.collect.Iterables.getFirst;
+import static com.google.common.collect.Iterables.size;
+import static java.util.Arrays.asList;
+import static org.hamcrest.MatcherAssert.*;
+import static org.hamcrest.Matchers.*;
+
+import cc.broomwagon.model.page.Page;
+import cc.broomwagon.web.ui.PageForm;
+import cc.broomwagon.web.ui.RowForm;
+import cc.broomwagon.web.ui.SegmentForm;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.runners.MockitoJUnitRunner;
+
+@RunWith(MockitoJUnitRunner.class)
+public class PageTranslatorTest {
+    @InjectMocks
+    private PageTranslator pageTranslator;
+
+    @SuppressWarnings("ConstantConditions")
+    @Test
+    public void shouldTranslate() {
+        // given
+        PageForm pageForm = new PageForm();
+        pageForm.setId(1L);
+        pageForm.setTitle("title");
+        pageForm.setUrl("url");
+
+        SegmentForm segmentForm = new SegmentForm();
+        segmentForm.setId(1L);
+        segmentForm.setFragment("fragment");
+        segmentForm.setTemplate("template");
+        segmentForm.setCssClass("css");
+
+        RowForm rowForm = new RowForm();
+        rowForm.setSegments(asList(segmentForm, segmentForm, segmentForm));
+
+        Iterable<RowForm> rows = asList(rowForm, rowForm);
+
+        pageForm.setRows(rows);
+
+        // when
+        Page actual = pageTranslator.translate(pageForm);
+
+        // then
+        assertThat(actual, is(notNullValue()));
+        assertThat(actual.getId(), is(1L));
+        assertThat(actual.getUrl(), is("url"));
+        assertThat(actual.getTitle(), is("title"));
+
+        assertThat(size(actual.getRows()), is(2));
+        assertThat(size(getFirst(actual.getRows(), null).getSegments()), is(3));
+        assertThat(getFirst(getFirst(actual.getRows(), null).getSegments(), null).getCssClass(), is("css"));
+    }
+
+}
